9bd356f4daab5a3af3ad206794f91d598d357d43
iknowright <changchaishi@gmail.com>
2021-05-06 15:42:43 +0800

    feat: reset app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import Web3 from 'web3';
+
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="Ethereum-logo" alt="logo" />
+      <h1 class="text-">Blockchain Explorer</h1>
+      <p class="text-left"><span>Network ID: </span><span class="networkId"></span></p>
+      <p class="text-left"><span>Peers: </span><span class="peerCount"></span></p>
+      <p class="text-left stats"></p>
+      <h3> Latest 20 blocks </h3>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">TxHash</th>
+            <th scope="col">Block</th>
+            <th scope="col">Timestamp</th>
+            <th scope="col">Gas Used</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
     </div>
   );
 }
